830848b72c649124e10d14150a86ca4b63ff5964
Kole Johnson <Flamingcoder05@gmail.com>
2025-01-27 20:10:53 -0600

    Important changes made!

--- src/main/java/net/wise/drstonemod/DrStoneMod.java
+++ src/main/java/net/wise/drstonemod/DrStoneMod.java
@@ -21,7 +21,7 @@ public class DrStoneMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "drstonemod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public DrStoneMod(FMLJavaModLoadingContext context)
     {
