50c1d01615eb5bf6fd5b41c6e367753fa9efa994
Kole Johnson <Flamingcoder05@gmail.com>
2025-03-22 17:58:37 -0500

    Added Tags for metal_detector_valuables

--- src/main/java/net/wise/drstonemod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/wise/drstonemod/item/custom/MetalDetectorItem.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.wise.drstonemod.util.ModTags;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties){
@@ -52,6 +53,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
---
+++ src/main/java/net/wise/drstonemod/util/ModTags.java
@@ -0,0 +1,30 @@
+package net.wise.drstonemod.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.wise.drstonemod.DrStoneMod;
+
+public class ModTags
+{
+    public static class Blocks
+    {
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
+        private static TagKey<Block> tag(String name)
+        {
+            return BlockTags.create(new ResourceLocation(DrStoneMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items
+    {
+        private static TagKey<Item> tag(String name)
+        {
+            return ItemTags.create(new ResourceLocation(DrStoneMod.MOD_ID, name));
+        }
+    }
+}
